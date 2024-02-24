58c93d752c373279b16d2b460fbc8416fccb761a
Bipin Saha <47634524+bipin-saha@users.noreply.github.com>
2024-02-24 09:45:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 import os
 import numpy as np
 import pandas as pd
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 from pklUnCompressor import decompress_pickle
 
 app = Flask(__name__)
@@ -12,27 +12,27 @@ output_pickle_file = 'uncompressed_forest.pkl'
 decompress_pickle(input_compressed_pickle_file, output_pickle_file)
 
 model = pickle.load(open('uncompressed_forest.pkl', 'rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
 
 
 @app.route('/')
 def home():
-
     return render_template("index.html")
+
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = model.predict(new_data)
     return jsonify(output[0])
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.form.to_dict(flat=False)
     data = {key: float(value[0]) for key, value in data.items()}
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    print(new_data)
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = model.predict(new_data)[0]
     return render_template("index.html", prediction_text=output)
 
