8e1ee0cb39bef7543fcaf546638738467bbe6a5e
bipin <bipin@ba-systems.com>
2024-02-25 09:36:39 +0600

    Update : Implement Model, Fix Uncompress Pickel Issue

--- app.py
+++ app.py
@@ -3,15 +3,38 @@ import os
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 from pklUnCompressor import decompress_pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import ExtraTreesRegressor
+
+califo = fetch_california_housing()
+
+dataset = pd.DataFrame(califo.data, columns=califo.feature_names)
+
+dataset['Price'] = califo.target
+
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+forest = ExtraTreesRegressor()
+forest.fit(X_train, y_train)
 
 app = Flask(__name__)
 
-input_compressed_pickle_file = 'compressed_forest.pkl.gz'
-output_pickle_file = 'uncompressed_forest.pkl'
-decompress_pickle(input_compressed_pickle_file, output_pickle_file)
+#input_compressed_pickle_file = 'compressed_forest.pkl.gz'
+#output_pickle_file = 'uncompressed_forest.pkl'
+#decompress_pickle(input_compressed_pickle_file, output_pickle_file)
 
-model = pickle.load(open('uncompressed_forest.pkl', 'rb'))
+model = forest
+#model = pickle.load(open('uncompressed_forest.pkl', 'rb'))
 scalar = pickle.load(open('scaling.pkl', 'rb'))
 
 
